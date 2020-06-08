5547479d9983e908761d8b909177124b10f8cc5d
Dean Erickson <dmepilot@yahoo.com>
2020-06-08 15:43:13 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,14 @@
 def my_collect(collection)
   if block_given?
+    new_collection = []
     i = 0 
     while 
       i < collection.length 
+      new_collection << 
       yield collection[i]
       i += 1 
+  end
+  new_collection
 end
 
+my_collect
