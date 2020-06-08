d0d536cf9562f2e7ff9848bbcbe35480a28eb20c
Dean Erickson <dmepilot@yahoo.com>
2020-06-08 15:59:13 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,8 +3,7 @@ def my_collect(array)
     new_collection = []
     i = 0 
     while i < array.length 
-      yield array[i]
-      new_collection << yield
+      new_collection << yield(array[i])
       i += 1 
   end
   new_collection
