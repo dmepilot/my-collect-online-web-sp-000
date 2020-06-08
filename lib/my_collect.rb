a4c298a1d91be60dfb8f14bd72755e426d7486ef
Dean Erickson <dmepilot@yahoo.com>
2020-06-08 15:56:10 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,7 @@ def my_collect(array)
   if block_given?
     new_collection = []
     i = 0 
-    while 
-      i < array.length 
+    while i < array.length 
       new_collection << yield array[i]
       i += 1 
   end
